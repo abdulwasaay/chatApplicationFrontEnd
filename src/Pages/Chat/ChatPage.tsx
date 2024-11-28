0f62f028f88e9b65aaaf876127f95551e57ecfe3
= <=>
2024-11-28 12:10:54 -0500

    done

--- src/Pages/Chat/ChatPage.tsx
+++ src/Pages/Chat/ChatPage.tsx
@@ -1,15 +1,14 @@
 
 import { useContext } from "react";
-// import ChatComp from "../../Components/ChatComponent/Chat"
+import ChatComp from "../../Components/ChatComponent/Chat"
 import AppBarContainer from "../../Components/MainAppBar/AppBarContainer"
 import { UserContext } from "../../Context/UserContext";
-// import Landing from "../../Components/WelcomeCompLatest/Landing";
+import Landing from "../../Components/WelcomeCompLatest/Landing";
 
 
 const ChatPage = () => {
     const smScreen = window.innerWidth <= 639;
     const { isHide, user } = useContext<any>(UserContext);
-    console.log(user)
 
     return (
         <div className=" sm:flex sm:gap-4 sm:overflow-x-auto md:overflow-x-visible">
@@ -17,24 +16,24 @@ const ChatPage = () => {
                 smScreen ? (
                     <>
                         {!isHide && <AppBarContainer type="chat" />}
-                        {/* {
+                        {
                             user ? (
                                 isHide && <ChatComp />
                             ) : (
                                 <Landing />
                             )
-                        } */}
+                        }
                     </>
                 ) : (
                     <>
                         <AppBarContainer type="chat" />
-                        {/* {
+                        {
                             user ? (
                                 <ChatComp />
                             ) : (
                                 <Landing />
                             )
-                        } */}
+                        }
                     </>
                 )
             }
