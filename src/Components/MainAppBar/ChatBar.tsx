c1b92cd7d23974429d796d818ef3005141c70935
= <=wasaysaleem071@gmail.com>
2024-12-02 07:19:21 +0500

    done

--- src/Components/MainAppBar/ChatBar.tsx
+++ src/Components/MainAppBar/ChatBar.tsx
@@ -4,118 +4,117 @@
 
 import "./styles.css"
 
-// import { appColour } from "../../constants/colours"
-// import InputField from "../inputFields/InputField"
-// import { useFormik } from "formik"
-// import profileColors from "../../constants/ProfileColorScheme"
-// import { useContext } from "react"
-// import { UserContext } from "../../Context/UserContext"
+import { appColour } from "../../constants/colours"
+import InputField from "../inputFields/InputField"
+import { useFormik } from "formik"
+import profileColors from "../../constants/ProfileColorScheme"
+import { useContext } from "react"
+import { UserContext } from "../../Context/UserContext"
 
 const ChatBar = ({ }) => {
 
-    // const { setUser, setIsHide } = useContext<any>(UserContext);
+    const { setUser, setIsHide } = useContext<any>(UserContext);
 
-    // const chatsArr = [
-    //     {
-    //         name: "Dummy user1",
-    //         currentMess: "I'm so happy today",
-    //         isOnline: true
-    //     }, {
-    //         name: "Dummy user2",
-    //         currentMess: "shas dsjh jd dsdsjj sdd dsjhh s ds dd sdsdsj dsdn dnd kjhwe ss dhnw w 2323",
-    //         isOnline: true
-    //     }, {
-    //         name: "Dummy user3",
-    //         currentMess: "",
-    //         isOnline: false
-    //     }, {
-    //         name: "Dummy user4",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user5",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user6",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user7",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: false
-    //     },
-    //     {
-    //         name: "Dummy user8",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user9",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user10",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: false
-    //     },
-    //     {
-    //         name: "Dummy user11",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user12",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user13",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: false
-    //     }, {
-    //         name: "Dummy user14",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
-    //     {
-    //         name: "Dummy user15",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     }, {
-    //         name: "Dummy user16",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     }, {
-    //         name: "Dummy user17",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     }, {
-    //         name: "Dummy user18",
-    //         currentMess: "Hello, How are you?",
-    //         isOnline: true
-    //     },
+    const chatsArr = [
+        {
+            name: "Dummy user1",
+            currentMess: "I'm so happy today",
+            isOnline: true
+        }, {
+            name: "Dummy user2",
+            currentMess: "shas dsjh jd dsdsjj sdd dsjhh s ds dd sdsdsj dsdn dnd kjhwe ss dhnw w 2323",
+            isOnline: true
+        }, {
+            name: "Dummy user3",
+            currentMess: "",
+            isOnline: false
+        }, {
+            name: "Dummy user4",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user5",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user6",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user7",
+            currentMess: "Hello, How are you?",
+            isOnline: false
+        },
+        {
+            name: "Dummy user8",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user9",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user10",
+            currentMess: "Hello, How are you?",
+            isOnline: false
+        },
+        {
+            name: "Dummy user11",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user12",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user13",
+            currentMess: "Hello, How are you?",
+            isOnline: false
+        }, {
+            name: "Dummy user14",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
+        {
+            name: "Dummy user15",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        }, {
+            name: "Dummy user16",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        }, {
+            name: "Dummy user17",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        }, {
+            name: "Dummy user18",
+            currentMess: "Hello, How are you?",
+            isOnline: true
+        },
 
-    // ];
+    ];
 
-    // const searchFormik = useFormik({
-    //     initialValues: { chatSearch: "" },
-    //     onSubmit: ((values: any) => {
-    //         console.log(values)
-    //     })
-    // })
+    const searchFormik = useFormik({
+        initialValues: { chatSearch: "" },
+        onSubmit: ((values: any) => {
+            console.log(values)
+        })
+    })
 
-    // const searchName = searchFormik?.values?.chatSearch.replace(/\s/g, "").toLowerCase();
-    // const filteredArr = chatsArr?.filter((chat: any) => chat?.name?.replace(/\s/g, "").toLowerCase().startsWith(searchName));
+    const searchName = searchFormik?.values?.chatSearch.replace(/\s/g, "").toLowerCase();
+    const filteredArr = chatsArr?.filter((chat: any) => chat?.name?.replace(/\s/g, "").toLowerCase().startsWith(searchName));
 
     return (
-        <div className="h-[100vh] w-16 border flex flex-col justify-end">
-            <h1>ds</h1>
-            {/* <div className="flex justify-center pl-5 pr-5">
+        <div className="h-[100vh] relative w-full sm:w-[360px] flex flex-col items-center ">
+            <div className=" absolute w-full text-center pl-3 pr-3 sm:pl-0 sm:pr-0" style={{ zIndex: "3" }}>
                 <InputField
                     name="chatSearch"
                     types="search"
@@ -125,11 +124,11 @@ const ChatBar = ({ }) => {
                     formik={searchFormik}
                 />
             </div>
-            <div className=" pt-10 h-[100%]" >
-                <div className="custom-scrollbar-style w-full h-full flex flex-col items-center gap-4  overflow-x-hidden pl-5 pr-5">
+            <div className="h-[100vh] w-full relative pb-5" style={{ zIndex: "1" , paddingTop:"80px" }}>
+                <div className="custom-scrollbar-style w-full h-full flex flex-col items-center gap-4 overflow-x-hidden pl-5 pr-5">
                     {filteredArr?.map((chat: any) => (
                         <div
-                            className="bg-[#3f4396] flex w-full sm:w-[300px] items-center gap-4 p-3 rounded-md cursor-pointer"
+                            className="bg-[#3f4396] flex w-full  sm:w-[300px] items-center gap-4 p-3 rounded-md cursor-pointer"
                             onClick={() => {
                                 setUser(chat);
                                 setIsHide(true);
@@ -159,7 +158,7 @@ const ChatBar = ({ }) => {
                         </div>
                     ))}
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
